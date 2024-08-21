48becce2f7525735d421a083dd901bf69739325b
Tenefou <gaspar.rg69@gmail.com>
2024-08-22 01:49:33 +0200

    Map

--- Assets/_Project/Scripts/Runtime/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Runtime/Player/PlayerMovement.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInput : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     PlayerInput input;
-    
+    InputAction moveAction;
+
+    [SerializeField] private float speed = 10.0f;
+
+    private void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        moveAction = input.actions.FindAction("Move");
+    }
+
+    void MovePlayer()
+    {
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
 }
